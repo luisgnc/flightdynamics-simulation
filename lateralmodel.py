8e2ff2d4559d58e07674d4e627314f19e64fe737
Luís Gonçalves <luisgvs0401@gmail.com>
2024-12-18 05:31:50 +0000

    Outputs
    changes to have better view of outputs

--- lateralmodel.py
+++ lateralmodel.py
@@ -1,6 +1,7 @@
 import sympy as sp
 import math
 
+
 #Flags
 i = 0
 
@@ -11,8 +12,14 @@ du, dalpha, dtheta, dq, d_delta_t,d_delta_e = sp.symbols('du, dalpha, dtheta, dq
 rho, S, L, C_L, C_D, T_max, epsilon_t, m = sp.symbols('rho, S, L, C_L, C_D, T_max, epsilon_t, m')
 c_media, C_M, I_yy = sp.symbols('c_media, C_M, I_yy')
 
+x1_dot, x2_dot, x3_dot, x4_dot = sp.symbols('x1_dot, x2_dot, x3_dot, x4_dot')
+
+x_symbol = sp.Matrix([u, alpha, theta, q])
+u_symbol = sp.Matrix([delta_t, delta_e])
+y_symbol = sp.Matrix([u, theta, -alpha, q])
 
 #cte SI UNITS
+'''
 u_0 = 0
 alpha_0 = 0
 theta_0 = 0
@@ -23,6 +30,8 @@ u_1 = sp.Matrix([0.5, 0])
 u_2 = sp.Matrix([0.5, 0.1])
 u_3 = sp.Matrix([0.5, 0])
 u_4 = sp.Matrix([0.25, 0])
+
+'''
 
 
 g = 9.80665
@@ -35,9 +44,7 @@ if h < 11000:
 else:
     rho_h = 0.29707 * rho_ref * math.e **((1.576939464 * 10 ** -4) * (11000 - h))
 
-
 #Matrizes Constantes
-
 A = sp.Matrix([[0.017, 19.03, -32.193, 0],
               [-0.0005, -1.56, 0.0056, 0.612],
               [0, 0, 0, 1],
@@ -52,16 +59,10 @@ vp_A = sp.Matrix(A).eigenvals()
 
 # Lista do tipo [Real1, Imaginario1][Real2, Imaginario 2]
 vp_A_lista = list()
-
 # Dicionario tipo Tpi: Value, w_ni: Value, zeta_i: Value
 controlval = dict()
 
 for chave, valor in vp_A.items():
-    new = list()
-    new.append(sp.re(chave))
-    new.append(sp.im(chave))
-    vp_A_lista.append(new)
-
     if i % 2 == 0:
         w = (sp.re(chave)** 2 + sp.im(chave)** 2) ** 0.5
         zeta = -sp.re(chave)/w
@@ -70,14 +71,29 @@ for chave, valor in vp_A.items():
         controlval[f'Tp_{i}'] = tp
         controlval[f'w_n{i}'] = w
         controlval[f'zeta_{i}'] = zeta
-
     i += 1
+
+for k, v in controlval.items():
+    if 'Tp' in k:
+        if v > 10:
+            print('\nMovimento Fugóide')
+        else:
+            print('\nMovimento de Período Curto')
+    print(f"{k} = {v}")
+
+
+# Modelo Linearizado
+x_dot = sp.Matrix([x1_dot, x2_dot, x3_dot, x4_dot])
+x_dot = A*x_symbol + B*u_symbol
 
 print(controlval)
 
-
-
 '''
+    new = list()
+    new.append(sp.re(chave))
+    new.append(sp.im(chave))
+    vp_A_lista.append(new)
+
 def longitudinal_model(x, u):
     du = (1/m)*(0.5*rho*S * (u**2/(sp.cos(alpha))**2) * (C_L*sp.sin(alpha) - C_D*sp.cos(alpha)) +
                 delta_t*T_max*sp.cos(epsilon_t)) - g*sp.sin(theta) - q*u*sp.tan(alpha)
